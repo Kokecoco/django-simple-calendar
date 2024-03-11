349fb3e9ad7370b0b539dd32777975a107111a0a
Kokecoco <gs2312160@gll-kaisei-s.sapporo-c.ed.jp>
2024-03-11 19:33:13 +0900

    deleted:    app/templates/app/month.html deleted:    app/templates/app/month_with_schedule.html modified:   app/templates/app/mycalendar.html deleted:    app/templates/app/week.html deleted:    app/templates/app/week_with_schedule.html modified:   app/urls.py modified:   app/views.py

--- app/views.py
+++ app/views.py
@@ -5,55 +5,6 @@ from .forms import BS4ScheduleForm, SimpleScheduleForm
 from .models import Schedule
 from . import mixins
 
-
-class MonthCalendar(mixins.MonthCalendarMixin, generic.TemplateView):
-    """月間カレンダーを表示するビュー"""
-    template_name = 'app/month.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        calendar_context = self.get_month_calendar()
-        context.update(calendar_context)
-        return context
-
-
-class WeekCalendar(mixins.WeekCalendarMixin, generic.TemplateView):
-    """週間カレンダーを表示するビュー"""
-    template_name = 'app/week.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        calendar_context = self.get_week_calendar()
-        context.update(calendar_context)
-        return context
-
-
-class WeekWithScheduleCalendar(mixins.WeekWithScheduleMixin, generic.TemplateView):
-    """スケジュール付きの週間カレンダーを表示するビュー"""
-    template_name = 'app/week_with_schedule.html'
-    model = Schedule
-    date_field = 'date'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        calendar_context = self.get_week_calendar()
-        context.update(calendar_context)
-        return context
-
-
-class MonthWithScheduleCalendar(mixins.MonthWithScheduleMixin, generic.TemplateView):
-    """スケジュール付きの月間カレンダーを表示するビュー"""
-    template_name = 'app/month_with_schedule.html'
-    model = Schedule
-    date_field = 'date'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        calendar_context = self.get_month_calendar()
-        context.update(calendar_context)
-        return context
-
-
 class MyCalendar(mixins.MonthCalendarMixin, mixins.WeekWithScheduleMixin, generic.CreateView):
     """月間カレンダー、週間カレンダー、スケジュール登録画面のある欲張りビュー"""
     template_name = 'app/mycalendar.html'
