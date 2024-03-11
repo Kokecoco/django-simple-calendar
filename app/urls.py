cc4891b5b19f9add68a1f880d4f460a4cdf42288
Kokecoco <gs2312160@gll-kaisei-s.sapporo-c.ed.jp>
2024-03-11 19:44:29 +0900

    revival month_with_schedule

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,14 @@ from . import views
 app_name = 'app'
 
 urlpatterns = [
+    path(
+        'month_with_schedule/',
+        views.MonthWithScheduleCalendar.as_view(), name='month_with_schedule'
+    ),
+    path(
+        'month_with_schedule/<int:year>/<int:month>/',
+        views.MonthWithScheduleCalendar.as_view(), name='month_with_schedule'
+    ),
     path('mycalendar/', views.MyCalendar.as_view(), name='mycalendar'),
     path(
         'mycalendar/<int:year>/<int:month>/<int:day>/', views.MyCalendar.as_view(), name='mycalendar'
--- app/views.py
+++ app/views.py
@@ -5,6 +5,18 @@ from .forms import BS4ScheduleForm, SimpleScheduleForm
 from .models import Schedule
 from . import mixins
 
+class MonthWithScheduleCalendar(mixins.MonthWithScheduleMixin, generic.TemplateView):
+    """スケジュール付きの月間カレンダーを表示するビュー"""
+    template_name = 'app/month_with_schedule.html'
+    model = Schedule
+    date_field = 'date'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        calendar_context = self.get_month_calendar()
+        context.update(calendar_context)
+        return context
+
 class MyCalendar(mixins.MonthCalendarMixin, mixins.WeekWithScheduleMixin, generic.CreateView):
     """月間カレンダー、週間カレンダー、スケジュール登録画面のある欲張りビュー"""
     template_name = 'app/mycalendar.html'
